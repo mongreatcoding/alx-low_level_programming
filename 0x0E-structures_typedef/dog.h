d9653f0bd0e855a235b844bd24f1396ae950d49e
mongreatcoding <augustineagge@gmail.com>
2023-01-16 07:31:26 -0800

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,26 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef DOG_H
+#define DOG_H
 
-#define SIZE 1024
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-#endif /*OBJECT_LIKE_MACRO_H*/
+/**
+ * struct dog - structure for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: pet's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
